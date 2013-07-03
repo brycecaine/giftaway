2c04ed3d776d99c370a8099f267ed72bc8ab5a08
Bryce Caine <cainebryce@gmail.com>
2013-07-02 19:41:50 -0600

    More static file config

--- giftaway/urls.py
+++ giftaway/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^api/', include(gift_resource.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
