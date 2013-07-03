1cc7f4e482f1e26a66b568ef4f6279cd35c9b2b9
Bryce Caine <cainebryce@gmail.com>
2013-07-02 19:44:27 -0600

    More static file config

--- giftaway/urls.py
+++ giftaway/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from giftplanner.api import GiftResource
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
